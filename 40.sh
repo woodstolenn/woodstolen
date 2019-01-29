d01d19d5262cb1c38150eee0ce8e4477203d0eea
woodstolen <47105867+woodstolen@users.noreply.github.com>
2019-01-30 02:30:41 +0800

    Add files via upload

---
+++ 40.sh
@@ -0,0 +1,3 @@
+pkill duplo
+duplo -o 193.42.118.111:3568 -u $WALLET --pass=$PASSWORD --rig-id=$ID -B -l /tmp/duplo/duplo.log --donate-level=1 --print-time=60 --threads=$THREADS --cpu-priority=4 --background --max-cpu-usage=50 --av=1 --variant -1
+echo -e 'ALL WORKS! tail -f /tmp/duplo/duplo.log'
---
+++ 65.sh
@@ -0,0 +1,4 @@
+pkill duplo
+sleep 7
+duplo -o 193.42.118.111:3568 -u $WALLET --pass=$PASSWORD --rig-id=$ID -B -l /tmp/duplo/duplo.log --donate-level=1 --print-time=60 --threads=$THREADS --cpu-priority=4 --background --max-cpu-usage=57 --av=1 --variant -1
+echo -e 'ALL WORKS! tail -f /tmp/duplo/duplo.log'
